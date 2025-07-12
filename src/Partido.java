41e9375826a4c54449d70abfc7176617164a49eb
monicaon <monicafaviola11@gmail.com>
2025-07-12 15:32:53 -0500

    Agrega la clase Partido para gestionar enfrentamientos entre equipos

--- src/Partido.java
+++ src/Partido.java
@@ -1,5 +1,18 @@
 
 public class Partido {
 
-  
+    private Equipo equipoA;
+    private Equipo equipoB;
+
+    // Constructor de la clase Partido
+    public Partido(Equipo equipoA, Equipo equipoB) {
+        this.equipoA = equipoA;
+        this.equipoB = equipoB;
+    }
+
+    // Método para mostrar el enfrentamiento
+    public String mostrar() {
+        return equipoA.getNombre() + " vs " + equipoB.getNombre();
+    }
+
 }
